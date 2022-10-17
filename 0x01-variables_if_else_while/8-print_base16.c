1f7084715961c5b637e6f55313288707c7fe9757
chidindu <obipeace87@gmail.com>
2022-10-17 16:43:45 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,11 +13,9 @@ int main(void)
 	{
 		putchar(i);
 	}
-	char alp[6] = "abcdef";
-
-	for (i = 0; i < 6; i++)
+	for (i = 97; i < 103; i++)
 	{
-		putchar(alp[i]);
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
