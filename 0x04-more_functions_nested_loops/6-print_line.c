c7ed7c2378b6c9d03506ace7e3ef6da6f6e20c24
chidindu <obipeace87@gmail.com>
2022-10-20 15:03:39 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,11 +7,11 @@
 */
 void print_line(int n)
 {
-	if (n > 0)
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
-		_putchar(n * 95);
+		_putchar(95);
 	}
-	else if (n <= 0)
-		_putchar('\n');
 	_putchar('\n');
 }
