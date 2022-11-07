47e7b48c23ed4629a8e73f98352034987adf0b9a
chidindu <obipeace87@gmail.com>
2022-11-07 06:44:24 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,6 +10,9 @@ int main(int argc, char *argv[])
 {
 	if (argc == 3)
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	printf("%s\n", Error);
-	return (1);
+	else
+	{
+		printf("%s\n", Error);
+		return (1);
+	}
 }
