5ae1a26bce406752df31cf21cb8748b51b6717b3
chidindu <obipeace87@gmail.com>
2022-11-07 06:08:06 -0800

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,12 +9,8 @@
 int main(int argc, char *argv[])
 {
 	int i;
-
-	printf("%s\n", argv[0]);
-	if (i > 1)
-	{
-		for (i = 1; i < argc; i++)
+	
+	for (i = 0; i < argc; i++)
 			printf("%s\n", argv[i]);
-	}
 	return (0);
 }
