c583a065286b96c511b304fb959113848e2afed9
chidindu <obipeace87@gmail.com>
2022-12-05 10:34:51 -0800

    init

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,13 +8,15 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, nletters, rwr;
+	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-		return (-1)<F9>;
+		return (-1);
 	if (!text_content)
 		text_content = "";
 	for (nletters = 0; text_content[nletters]; nletters++)
